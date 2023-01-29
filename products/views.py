86bc3ed13533d7b2ac872af72b087c1b06ead138
Kay-ddggxh <kathrin.welfare@gmail.com>
2023-01-29 16:33:30 +0000

    add view and template to filter products by category

--- products/views.py
+++ products/views.py
@@ -2,14 +2,35 @@ from django.shortcuts import render
 from .models import Product, Category
 
 
-def all_products(request):
+def all_categories(request):
     """
     Renders all poultry procucts
     """
+
     categories = Category.objects.all()
 
     context = {
         'categories': categories,
+    }
+
+    return render(request, 'products/categories.html', context)
+
+
+def products(request):
+    """
+    Renders all products of same category
+    """
+    products = Product.objects.all()
+    category_select = request.GET['category']
+
+    if request.GET:
+        if 'category' in request.GET:
+            category_select = request.GET['category']
+            products = products.filter(category__name=category_select)
+
+    context = {
+        'products': products,
+        'current_category': category_select,
     }
 
     return render(request, 'products/products.html', context)
