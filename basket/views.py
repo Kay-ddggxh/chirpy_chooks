cf47e4cb375f1acb97fdd62c37de411d7a25252f
Kay-ddggxh <kathrin.welfare@gmail.com>
2023-02-05 18:02:14 +0000

    complete remove functionality in shopping basket

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def basket(request):
@@ -8,7 +8,7 @@ def basket(request):
     return render(request, 'basket/basket.html')
 
 
-# Source code: https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/21e3b111100f5c8daa4bc6e9565bde09980b33d8/bag/views.py#:~:text=def%20add_to_bag(,(redirect_url)  # noqa
+# Source code: https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/21e3b111100f5c8daa4bc6e9565bde09980b33d8/basket/views.py#:~:text=def%20add_to_bag(,(redirect_url)  # noqa
 def add_to_basket(request, item_id):
     """
     allows items to be added to basket
@@ -39,3 +39,18 @@ def adjust_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(reverse('basket'))
+
+
+def remove_from_basket(request, item_id):
+    """Remove items from shopping basket"""
+
+    try:
+        basket = request.session.get('basket', {})
+
+        basket.pop(item_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
