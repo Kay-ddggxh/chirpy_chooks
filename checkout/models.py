d10939a3de683cb357672e41223749a036ab859c
Kay-ddggxh <kathrin.welfare@gmail.com>
2023-02-19 14:53:09 +0000

    add profiles app

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models import Sum
 
 from products.models import Product
+from profiles.models import UserProfile
+
 
 PLACEHOLDER = 'County *'
 CARLOW = 'Carlow'
@@ -67,6 +69,9 @@ COUNTIES = [
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL,
+        null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
